6ce326f1f5b57c517af567f5b12319ca444a8bf6
erica <heejaerica@gmail.com>
2021-04-13 00:03:06 -0700

    Final version2

--- client/src/MedicalStaff.jsx
+++ client/src/MedicalStaff.jsx
@@ -176,143 +176,137 @@ function MedicalStaff() {
           <div className="d-flex justify-content-center p-3">
             <h3>Create Schedule</h3>
           </div>
-          <div className="row">
-            <div className="column"></div>
-            <div className="column ">
-              <div className="d-flex justify-content-center">
-                <div className="createScehdule">
-                  <form className="d-flex justify-content-center ">
-                    <div className="form-row w-100 mx-3">
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputName4">Staff Name</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="inputName4"
-                          placeholder="name"
-                          onChange={(e) => {
-                            setName(e.target.value);
-                          }}
-                        />
-                      </div>
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputPosition4">Position</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="inputPosition4"
-                          placeholder="position"
-                          onChange={(e) => {
-                            setPosition(e.target.value);
-                          }}
-                        />
-                      </div>
-                      <div className="form-group col-md-6">
-                        {/* <label htmlFor="inputPosition4">Select Patient</label> */}
-                        <Dropdown>
-                          <span>Select patient: </span>
 
-                          <Dropdown.Toggle variant="success btn-sm" id="dropdown-basic">
-                            Patient Id
-                          </Dropdown.Toggle>
-                          <br />
+          <div className="d-flex justify-content-center">
+            <div className="createScehdule">
+              <form className="d-flex justify-content-center ">
+                <div className="form-row w-100 mx-3">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="inputName4">Staff Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputName4"
+                      placeholder="name"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="inputPosition4">Position</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputPosition4"
+                      placeholder="position"
+                      onChange={(e) => {
+                        setPosition(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    {/* <label htmlFor="inputPosition4">Select Patient</label> */}
+                    <Dropdown>
+                      <span>Select patient: </span>
 
-                          <Dropdown.Menu variant="secondary btn-sm" id="dropdown-basic">
-                            {patientList &&
-                              patientList.map((patient, index) => (
-                                <Dropdown.Item
-                                  key={index}
-                                  onClick={(e) => {
-                                    setselectedPatient(patient.date);
-                                    setStartTime(patient.date);
-                                    setPatientId(patient.ID);
-                                    setPatientState(patient.reservedState);
-                                  }}
-                                >
-                                  {console.log(patient)}
-                                  Patient Name: {patient.name}
-                                  <span>
-                                    {patient.reservedState === 1 ? (
-                                      <p>Reserved</p>
-                                    ) : (
-                                      <p>Not Reserved</p>
-                                    )}
-                                  </span>
-                                </Dropdown.Item>
-                              ))}
-                          </Dropdown.Menu>
-                        </Dropdown>
-                      </div>
+                      <Dropdown.Toggle variant="success btn-sm" id="dropdown-basic">
+                        Patient Id
+                      </Dropdown.Toggle>
+                      <br />
 
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputName4">Start Date:</label>
-                        <div>{selectedPatient}</div>
-                      </div>
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputName4">End Date: </label>
-                        <input
-                          type="date"
-                          className="form-control"
-                          onChange={(e) => {
-                            setEndDate(e.target.value);
-                          }}
-                        />
-                        <input
-                          type="time"
-                          className="form-control"
-                          onChange={(e) => {
-                            setEndTime(e.target.value);
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </form>
+                      <Dropdown.Menu variant="secondary btn-sm" id="dropdown-basic">
+                        {patientList &&
+                          patientList.map((patient, index) => (
+                            <Dropdown.Item
+                              key={index}
+                              onClick={(e) => {
+                                setselectedPatient(patient.date);
+                                setStartTime(patient.date);
+                                setPatientId(patient.ID);
+                                setPatientState(patient.reservedState);
+                              }}
+                            >
+                              {console.log(patient)}
+                              Patient Name: {patient.name}
+                              <span>
+                                {patient.reservedState === 1 ? (
+                                  <p>Reserved</p>
+                                ) : (
+                                  <p>Not Reserved</p>
+                                )}
+                              </span>
+                            </Dropdown.Item>
+                          ))}
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
 
-                  <br />
-                  <br />
-                  <div className="d-flex justify-content-center">
-                    <Button className="btn btn-success btn-sm mr-3" onClick={GetMedicalStaff}>
-                      View Schedule
-                    </Button>
-                    <Button className="btn btn-primary btn-sm mr-3" onClick={RegisterRequest}>
-                      Add Schedule
-                    </Button>
-                    <Button
-                      className="btn btn-info btn-sm mr-3"
-                      ref={target}
-                      onClick={() => setShow(!show)}
-                    >
-                      How To Use
-                    </Button>
-                    <Overlay target={target.current} show={show} placement="top">
-                      {(props) => (
-                        <Tooltip {...props}>
-                          <div style={{ lineHeight: "1" }}>
-                            <h5>Guide line</h5>
-                            <p>
-                              <b>Create Schedule:</b>
-                            </p>
-                            <p>1. Please fill up empty section and select one of patient</p>
-                            <p>2. Press Add Schedule button(blue colour button) </p>
-                            <p>
-                              <b>Update Schedule:</b>{" "}
-                            </p>
-                            <p>1. Please fill up empty sections </p>
-                            <p>2. Press Update button(grey colour button) </p>
-                            <p>
-                              <b>Delete Schedule:</b>{" "}
-                            </p>
-                            <p>Press Delete button(light blue colour button) </p>
-                          </div>
-                        </Tooltip>
-                      )}
-                    </Overlay>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="inputName4">Start Date:</label>
+                    <div>{selectedPatient}</div>
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="inputName4">End Date: </label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      onChange={(e) => {
+                        setEndDate(e.target.value);
+                      }}
+                    />
+                    <input
+                      type="time"
+                      className="form-control"
+                      onChange={(e) => {
+                        setEndTime(e.target.value);
+                      }}
+                    />
                   </div>
                 </div>
+              </form>
+
+              <br />
+              <br />
+              <div className="d-flex justify-content-center">
+                <Button className="btn btn-success btn-sm mr-3" onClick={GetMedicalStaff}>
+                  View Schedule
+                </Button>
+                <Button className="btn btn-primary btn-sm mr-3" onClick={RegisterRequest}>
+                  Add Schedule
+                </Button>
+                <Button
+                  className="btn btn-info btn-sm mr-3"
+                  ref={target}
+                  onClick={() => setShow(!show)}
+                >
+                  How To Use
+                </Button>
+                <Overlay target={target.current} show={show} placement="top">
+                  {(props) => (
+                    <Tooltip {...props}>
+                      <div style={{ lineHeight: "1" }}>
+                        <h5>Guide line</h5>
+                        <p>
+                          <b>Create Schedule:</b>
+                        </p>
+                        <p>1. Please fill up empty section and select one of patient</p>
+                        <p>2. Press Add Schedule button(blue colour button) </p>
+                        <p>
+                          <b>Update Schedule:</b>{" "}
+                        </p>
+                        <p>1. Please fill up empty sections </p>
+                        <p>2. Press Update button(grey colour button) </p>
+                        <p>
+                          <b>Delete Schedule:</b>{" "}
+                        </p>
+                        <p>Press Delete button(light blue colour button) </p>
+                      </div>
+                    </Tooltip>
+                  )}
+                </Overlay>
               </div>
             </div>
-
-            <div className="column"></div>
           </div>
 
           <div className="d-flex justify-content-center">
